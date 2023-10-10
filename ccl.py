e80742561ef750c13f8bcb1f95f1338557a75aac
ella-litjens <144051727+ella-litjens@users.noreply.github.com>
2023-10-10 13:52:39 +0100

    Changes to be committed: 	modified:   __pycache__/filterdata.cpython-312.pyc 	new file:   ccl.py 	modified:   filterdata.py 	new file:   std_2_hist.png

---
+++ ccl.py
@@ -0,0 +1,20 @@
+"""This module carries out Connected Component Labelling (CCL)"""
+
+from astropy.io import fits
+import numpy as np
+import matplotlib.pyplot as plt
+import Astrocode as ac
+import filterdata as fd
+import cv2
+import numpy as np
+
+
+# Apply binary thresholding
+binary_image = (ac.data > fd.std_filter).astype('uint8') * 255
+
+# Show the binary image
+cv2.imshow('Binary Image', binary_image)
+
+# Wait for a key press and then close the window
+cv2.waitKey(0)
+cv2.destroyAllWindows() 
