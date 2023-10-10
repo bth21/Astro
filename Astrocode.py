ff38914e5629361dc44266db762aab58a84a48dc
ella-litjens <144051727+ella-litjens@users.noreply.github.com>
2023-10-10 10:13:28 +0100

    Changes to be committed: 	modified:   Astrocode.py

--- Astrocode.py
+++ Astrocode.py
@@ -6,8 +6,43 @@ Created on Fri Oct  6 10:23:23 2023
 @author: benhale
 """
 from astropy.io import fits
+import numpy as np
+import matplotlib.pyplot as plt
 
-#test
+# open astro data file and create an array of pixel values
+hdulist = fits.open(r"C:\Users\Administrator\GitHub\Astro\mosaic.fits")
+hdulist[0].header
+data = hdulist[0].data
+
+# plot histogram
+num_bins = 10
+plt.hist(data.flatten(),num_bins)
+plt.xlabel("Value")
+plt.ylabel("Pixel Value")
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+hdulist.close()
+
+
+
+
+
+
 
 
 
