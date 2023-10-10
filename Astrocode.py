3dc03d993f2cf6ff876ae24d1db08cf6a87ddb23
bth21 <benhale106@btinternet.com>
2023-10-10 11:47:34 +0100

    Update Astrocode.py

--- Astrocode.py
+++ Astrocode.py
@@ -9,6 +9,7 @@ import numpy as np
 import scipy as scipy
 import matplotlib.pyplot as plt
 from astropy.io import fits
+from scipy.optimize import curve_fit
 
 hdulist = fits.open("Fits_data/mosaic.fits")
 hdulist[0].header
@@ -17,40 +18,62 @@ hdulist[0].header
 
 data = hdulist[0].data
 
-num_bins = 10
-plt.hist(data.flatten(), num_bins)
-plt.xlabel('Value')
-plt.ylabel('Frequency')
-plt.show()
-
-
-#%%
-
-
 border_size = 130   #cropping border pixels from edge of image
-new_width = data.shape[1] - 2 * border_size
-new_height = data.shape[0] - 2 * border_size
+new_width = data.shape[1] - 2 * border_size  #removing pixels from both sides
+new_height = data.shape[0] - 2 * border_size #removing pixels from top and bottom
 
 trimmed_image = data[border_size:border_size + new_height, border_size:border_size + new_width]
 
+# Create a mask to filter values
+low_range = 3350
+high_range = 3500
+mask = (trimmed_image >= low_range) & (trimmed_image <= high_range)
 
-# Assuming you have your data in the variable 'data'
-# Create a mask to filter values greater than or equal to 10,000
-mask = trimmed_image <= 3700
 
-# Apply the mask to your data to exclude values less than 10,000
+# Apply the mask to your data to exclude values less than 3500 and greater than 3350
 filtered_data = trimmed_image[mask]
 
 # Specify the number of bins you want in your histogram
 num_bins = 50
 
 # Plot the histogram
-plt.hist(filtered_data.flatten(), bins=num_bins, density=True, alpha=0.75, color='blue')
+plt.hist(filtered_data.flatten(), bins = num_bins, alpha=0.75, color='blue')
 
 # Add labels and a title
 plt.xlabel('Pixel Value')
 plt.ylabel('Frequency')
-plt.title('Histogram (Values >= 10,000)')
+plt.title('Histogram')
+
+
+hist, bin_edges = np.histogram(filtered_data, bins = num_bins)  #finding peak value of the histogram with 1000 bins
+peak_bin_index = np.argmax(hist)
+peak_value = (bin_edges[peak_bin_index] * 2 + 1) / 2 #finding centre of bin
+#peak_value is 3418.25
+
+x_centres = (bin_edges[:-1] + bin_edges[1:]) / 2
+y_centres = hist
+
+def gaussian(x, amplitude, mean, stddev):
+    return amplitude * np.exp(-(x - mean)**2 / (2 * stddev**2))
+
+# Initial guess for the parameters (amplitude, mean, and standard deviation)
+initial_guess = [800000, 3418, np.std(x_centres)]
+
+# Perform the curve fitting
+fit_params, _ = curve_fit(gaussian, x_centres, y_centres, p0=initial_guess)
+
+amplitude_fit, mean_fit, stddev_fit = fit_params
+
+plt.scatter(x_centres, y_centres, label='Data', color='red')
+plt.plot(x_centres, gaussian(x_centres, *fit_params), label='Fit', color='red')
+
+plt.xlabel('Pixel Value')
+plt.ylabel('Frequency')
+#plt.title('Fitting a Gaussian Curve to Data')
+plt.legend()
+#plt.savefig('Astrohist_fit')
 
 # Show the plot
 plt.show()
+
+hdulist.close()
