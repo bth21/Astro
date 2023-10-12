402e43762f222915eccfc96e02e192072be59f8b
bth21 <benhale106@btinternet.com>
2023-10-12 11:52:15 +0100

    combined original and clean data

---
+++ binary.py
@@ -0,0 +1,24 @@
+#%%
+from astropy.io import fits
+import numpy as np
+import Astrocode as ac
+from skimage.measure import label
+
+'''Converting pixel data into binary data with 5stds above mean
+background value'''
+# Define the threshold value (3442 is 2std above mean)
+std = 2
+threshold = 3418.13 + std * 11.79
+
+# Apply the threshold
+binary_data = (ac.trimmed_image > threshold).astype(np.uint8)
+
+
+# Close the FITS file
+ac.hdulist.close()
+
+# Save the binary data to a binary file (e.g., a FITS file or a binary image)
+# Here, we save it as a new FITS file
+binary_image = fits.PrimaryHDU(binary_data)
+binary_image.writeto('binary_output.fits', overwrite=True)
+
---
+++ blooming.py
@@ -0,0 +1,52 @@
+#%%
+from astropy.io import fits
+import numpy as np
+from scipy import ndimage
+from skimage.measure import label, regionprops
+import Astrocode as ac
+
+
+labeled_data = label(ac.binary_data, connectivity = 1) #assigns unique label to each connected group of pixels
+num_labels_binary = np.max(labeled_data) #number of objects in binary data
+print(num_labels_binary)
+object_properties = regionprops(labeled_data)
+
+blooming_mask = np.zeros_like(ac.binary_data, dtype=bool) #creates mask - array of zeros same shape as binary_data
+
+threshold_area = 5000
+
+for prop in object_properties:
+    # Adjust the criteria as needed to identify blooming artifacts
+    if prop.area > threshold_area:
+        blooming_mask[labeled_data == prop.label] = True
+
+cleaned_data = ac.binary_data.copy()
+cleaned_data[blooming_mask] = 0  #applies mask
+cleaned_data_label = label(cleaned_data, connectivity = 1)
+num_labels_clean = np.max(cleaned_data_label) #number of objects in cleaned data
+print(num_labels_clean)
+
+output_file = 'cleaned_image.fits'
+
+# Create a FITS header
+header = fits.Header()
+header['COMMENT'] = 'Cleaned image data'
+
+# Save the cleaned data to the FITS file
+fits.writeto(output_file, cleaned_data, header=header, overwrite=True)
+
+
+ac.trimmed_image[ac.trimmed_image == 0] = 0 #replacing the background in the original image with zeros
+
+restored_data = ac.trimmed_image * cleaned_data #combining the original data with the cleaned data
+output_file = 'restored_image.fits'
+restored_data_label = label(restored_data, connectivity = 1)
+num_labels_restored = np.max(restored_data_label)
+print(num_labels_restored)
+
+header['COMMENT'] = 'Restored image data'
+
+# Save the restored data to the FITS file
+fits.writeto(output_file, restored_data, header=header, overwrite=True)
+
+
